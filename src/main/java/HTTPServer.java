7ea14bb15c891e4908fe99ad004c8811e9cc0eb5
ArneTroch <44033131+ArneTroch@users.noreply.github.com>
2020-04-03 15:56:43 +0200

    Account default constructor

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class HTTPServer implements Runnable {
 
@@ -12,14 +14,13 @@ public class HTTPServer implements Runnable {
     // port to listen connection
     private static final int PORT = 8080;
 
-    // verbose mode
-    private static final boolean verbose = true;
-
     // Keeping track of total amount of threads
     private static int threads = 0;
 
     // Client Connection via Socket Class
     private Socket connect;
+
+    private List<String> usedFiles = new LinkedList<>();
 
     private HTTPServer(Socket c) {
         connect = c;
@@ -27,35 +28,18 @@ public class HTTPServer implements Runnable {
 
     public static void main(String[] args) {
         try {
+            // Server Setup
             System.out.println("Performing setup of server... ");
-            System.out.println("Checking existence of accounts folder");
-            File directory = new File(ACCOUNTFOLDER);
-            if (!directory.exists()) {
-                directory.mkdir();
-                System.out.println("No accounts folder found! Creating new folder and files...");
-                Account account1 = new Account("Siemen", 0, "Oosterveldaan 3", 17, "Lies");
-                Account account2 = new Account("Arne", 15000, "Korte kopstraat 70", 2, "Cassandra");
-                Account account3 = new Account("Gliesje", 15, "Oosterveldlaan 3", 50, "Yelena");
+            ServerSocket serverConnect = serverSetup();
 
-                mapper.writeValue(new File(ACCOUNTFOLDER, "/Siemen.json"), account1);
-                mapper.writeValue(new File(ACCOUNTFOLDER, "/Arne.json"), account2);
-                mapper.writeValue(new File(ACCOUNTFOLDER, "/Gliesje.json"), account3);
-            } else {
-                System.out.println("Accounts folder found!");
-            }
-            ServerSocket serverConnect = new ServerSocket(PORT);
-            System.out.println("Setup completed! Server started.\nListening for connections on port: " + PORT + "...\n");
-
-            // we listen until user halts server execution
+            // Main loop of main thread
             while (true) {
                 HTTPServer myServer = new HTTPServer(serverConnect.accept());
 
-                if (verbose) {
-                    System.out.println("\n----------------------------------------------------");
-                    System.out.println("New connection opened. (" + new Date() + ")");
-                }
+                System.out.println("\n----------------------------------------------------");
+                System.out.println("New connection opened. (" + new Date() + ")");
 
-                // create dedicated thread to manage the client connection
+                // Creating new thread to handle incoming connection
                 Thread thread = new Thread(myServer);
                 thread.start();
             }
@@ -65,77 +49,65 @@ public class HTTPServer implements Runnable {
         }
     }
 
+    static private ServerSocket serverSetup() throws IOException {
+        System.out.println("Checking existence of accounts folder");
+        File directory = new File(ACCOUNTFOLDER);
+        if (!directory.exists()) {
+            directory.mkdir();
+            System.out.println("No accounts folder found! Creating new folder and files...");
+            Account account1 = new Account("Siemen", 0, "Oosterveldaan 3", 17, "Lies");
+            Account account2 = new Account("Arne", 15000, "Korte kopstraat 70", 2, "Cassandra");
+            Account account3 = new Account("Gliesje", 15, "Oosterveldlaan 3", 50, "Yelena");
+
+            mapper.writeValue(new File(ACCOUNTFOLDER, "/Siemen.json"), account1);
+            mapper.writeValue(new File(ACCOUNTFOLDER, "/Arne.json"), account2);
+            mapper.writeValue(new File(ACCOUNTFOLDER, "/Gliesje.json"), account3);
+        } else {
+            System.out.println("Accounts folder found!");
+        }
+        System.out.println("Setup completed! Server started.\nListening for connections on port: " + PORT + "...\n");
+        return new ServerSocket(PORT);
+    }
+
     public void run() {
+        // Managing of new connection by dedicated thread
         System.out.println("Starting new thread for connection from " + connect.getInetAddress() + ". Total running threads: " + ++threads);
-        // we manage our particular client connection
         BufferedReader in;
         PrintWriter out;
         BufferedOutputStream dataOut;
 
         try {
-            // we read characters from the client via input stream on the socket
             in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-            // we get character output stream to client (for headers)
             out = new PrintWriter(connect.getOutputStream());
-            // get binary output stream to client (for requested data)
             dataOut = new BufferedOutputStream(connect.getOutputStream());
 
-            boolean balance = true;
-            boolean goodRequest = true;
-
-            // get first line of the request from the client
+            // Reading incoming request and parsing input to get the command
             String input = in.readLine();
             System.out.println("Input: " + input);
             String[] split1 = input.split(" ");
             String[] split2 = split1[1].split("/");
-            String account = "";
-            String amount = "";
-            if (split2.length == 2) {
-                account = split2[1].split(" ")[0];
-            } else if (split2.length == 3) {
-                balance = false;
-                account = split2[1].split(" ")[0];
-                amount = split2[2].split(" ")[0];
-            } else {
-                System.out.println("Wrong input: " + input);
-                errorMessage(out, dataOut, "400", "Bad Request");
-                goodRequest = false;
-            }
 
-            if (goodRequest) {
-                account = account + ".json";
-                File accountFile = new File(ACCOUNTFOLDER, account);
-                // File - and account - doesn't exist
-                if (accountFile.createNewFile()) {
-                    System.out.println("Account " + account + " does not exist");
-                    errorMessage(out, dataOut, "404", "Not Found");
-                } else {
-                    // Returnen van de balance van een account
-                    if (balance) {
-                        System.out.println("Sending account information for account: " + accountFile.getPath());
-                        ObjectMapper objMapper = new ObjectMapper();
-                        Account accountClass = objMapper.readValue(accountFile, Account.class);
-                        out.println("HTTP/1.1 200 OK");
-                        out.println("Server: Java HTTP Server from Arne");
-                        out.println("Date: " + new Date());
-                        out.println("Content-type: json");
-                        out.println();
-                        out.flush();
-                        mapper.writeValue(dataOut, accountClass);
-                        dataOut.flush();
-                    } else {
-                        System.out.println("Changing balance of account " + account + "with " + amount);
-                        Account accountClass = mapper.readValue(accountFile, Account.class);
-                        if (amount.charAt(0) == '-') {
-                            accountClass.setBalance(accountClass.getBalance() - Integer.parseInt(amount.replaceAll("\\D+", "")));
-                        } else if (amount.charAt(0) == '+') {
-                            accountClass.setBalance(accountClass.getBalance() + Integer.parseInt(amount.replaceAll("\\D+", "")));
-                        } else {
-                            errorMessage(out, dataOut, "400", "Bad Request");
-                        }
-                        mapper.writeValue(accountFile, accountClass);
+            switch (split2[1].trim()) {
+                case ("info"):
+                    infoInput(out, dataOut, split2[2] + ".json");
+                    break;
+                case ("changeBalance"):
+                    infoChangeBalance(out, dataOut, split2[2] + ".json", split2[3]);
+                    break;
+                case ("add"):
+                    new InputStreamReader(connect.getInputStream());
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
                     }
-                }
+
+                    Account account = mapper.readValue(content.toString(), Account.class);
+                    infoAdd(out, account);
+                    break;
+                default:
+                    errorMessage(out, "400", "Bad Request");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -143,17 +115,107 @@ public class HTTPServer implements Runnable {
         threads--;
     }
 
-    private void errorMessage(PrintWriter out, OutputStream dataOut, String errorCode, String errorMessage) throws IOException {
+    private void errorMessage(PrintWriter out, String errorCode, String errorMessage) {
         System.out.println("Error while processing request! Error code and message: " + errorCode + " " + errorMessage);
         out.println("HTTP/1.1 " + errorCode + errorMessage);
         out.println("Server: Java HTTP Server from Arne");
         out.println("Date: " + new Date());
         out.println("Content-type: json");
-        String json = "{ \"Message\" : \"" + errorMessage + "\" }";
         out.println();
         out.flush();
+    }
 
-        dataOut.write(json.getBytes(), 0, json.getBytes().length);
-        dataOut.flush();
+    private void infoInput(PrintWriter out, OutputStream dataOut, String account) {
+        File accountFile = new File(ACCOUNTFOLDER, account);
+        if (validAccount(out, accountFile)) {
+            try {
+                while (usedFiles.contains(account)){
+                    System.out.println("Account " + account + " is already in use by another thread!");
+                    Thread.sleep(20);
+                }
+                usedFiles.add(account);
+                System.out.println("Sending account information for account: " + account);
+                ObjectMapper objMapper = new ObjectMapper();
+                Account accountClass;
+                accountClass = objMapper.readValue(accountFile, Account.class);
+                out.println("HTTP/1.1 200 OK");
+                out.println("Server: Java HTTP Server from Arne");
+                out.println("Date: " + new Date());
+                out.println("Content-type: json");
+                out.println();
+                out.flush();
+                mapper.writeValue(dataOut, accountClass);
+                dataOut.flush();
+                usedFiles.remove(account);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void infoChangeBalance(PrintWriter out, OutputStream dataOut, String account, String amount) {
+        File accountFile = new File(ACCOUNTFOLDER, account);
+        if (validAccount(out, accountFile)) {
+            try {
+                while (usedFiles.contains(account)){
+                    System.out.println("Account " + account + " is already in use by another thread!");
+                    Thread.sleep(20);
+                }
+                usedFiles.add(account);
+                System.out.println("Changing balance of account " + account + "with " + amount);
+                Account accountClass;
+                accountClass = mapper.readValue(accountFile, Account.class);
+                if (amount.charAt(0) == '-') {
+                    accountClass.setBalance(accountClass.getBalance() - Integer.parseInt(amount.replaceAll("\\D+", "")));
+                } else if (amount.charAt(0) == '+') {
+                    accountClass.setBalance(accountClass.getBalance() + Integer.parseInt(amount.replaceAll("\\D+", "")));
+                } else {
+                    errorMessage(out,"400", "Bad Request");
+                }
+                mapper.writeValue(accountFile, accountClass);
+                out.println("HTTP/1.1 200 OK");
+                out.println("Server: Java HTTP Server from Arne");
+                out.println("Date: " + new Date());
+                out.println("Content-type: json");
+                out.println();
+                out.flush();
+                usedFiles.remove(account);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void infoAdd(PrintWriter out, Account account) {
+        File accountFile = new File(ACCOUNTFOLDER, account.getName() + ".json");
+        if (!accountFile.isFile()) {
+            try {
+                mapper.writeValue(accountFile, account);
+                out.println("HTTP/1.1 200 OK");
+                out.println("Server: Java HTTP Server from Arne");
+                out.println("Date: " + new Date());
+                out.println("Content-type: json");
+                out.println();
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            errorMessage(out,"409", "Conflict");
+        }
+    }
+
+    private boolean validAccount(PrintWriter out, File accountFile) {
+        if (!accountFile.isFile()) {
+            System.out.println("Account " + accountFile.getName() + " does not exist");
+            errorMessage(out,"404", "Not Found");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
