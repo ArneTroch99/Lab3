05e8e5028665ed57a9be3709349fb647cf1f5a59
siemenHerremans <siemen_herremans@hotmail.com>
2020-04-18 11:02:14 +0000

    blabla

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -7,17 +7,19 @@ class HttpClient{
 	
 	private HttpURLConnection con;
 
-	private void openConnection(String address, String HttpMethod) throws Exception{
+	private void openConnection(String request, String method) throws Exception{
 
-		URL url = new URL(address);
+		URL url = new URL(request);
 		con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod(HttpMethod);
+		con.setRequestMethod(method);
 		con.setConnectTimeout(5000);
 		con.setReadTimeout(5000);
 	}
 
-	private Account getResponse() throws Exception{
+	public Account getAccount(String address, String name) throws Exception{
 		
+		String request = address + "/getAccount?account="+name;
+		openConnection(address, "GET");
 		BufferedReader in = new BufferedReader(
   		new InputStreamReader(con.getInputStream()));
 		String inputLine;
@@ -28,6 +30,7 @@ class HttpClient{
 		}
 		
 		in.close();
+		con.close();
 		// create object mapper instance
 		ObjectMapper mapper = new ObjectMapper();
 
@@ -41,34 +44,16 @@ class HttpClient{
 		String jsonInputString = objectMapper.writeValueAsString(account);
 
 		con.setRequestProperty("Content-Type", "application/json; utf-8");
+		//con.setRequestProperty("Accept", "text/plain");
 		con.setDoOutput(true);
-		
+			
 		try(OutputStream os = con.getOutputStream()) {
  		byte[] input = jsonInputString.getBytes("utf-8");
     		os.write(input, 0, input.length);           
 		}
-		return con.getResponseCode();	
-	} 
-
-	private void closeConnection() throws Exception{
-		con.disconnect();
-	}
-
-	public Account get(String address) throws Exception{
-		openConnection(address, "GET");
-		
-		Account account = getResponse();
-		closeConnection();
-		return account;
-	}
-
-	public void post(String address, Account account)throws Exception{
-		openConnection(address, "POST");
-		int error = sendAccount(account);
-		closeConnection();
-		
-		if (error == 200){System.out.println("File sucesfully added");return;}
-		System.out.println("File not added! Code: "+error);
+		//con.getInputStream().close();
+		//return con.getResponseCode();
+		return con.getResponseCode();
 	}	
 }
 	
--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -4,10 +4,7 @@ class MainClient{
 		
 		HttpClient client = new HttpClient();
 		
-		Account account = client.get("http://10.0.13.14:8080/info/siemen");
-		System.out.println(account.getName() + "  " + account.getBalance());
-		
-		account = new Account("Ronny", 20, "TestStraat 1", 3, "Benny");
-		client.post("http://10.0.13.14:8080/add", account);
+		Account account = client.getAccount("http://10.0.13.14:8080", "Siemen");
+		System.out.println(account.toString);
 	}
 }
